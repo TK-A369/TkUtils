462619b02ee5e58ce5106a9ed9705d71c55ca570
Tomasz <tkawalec369@gmail.com>
2019-05-03 21:20:54 +0200

    First commit

---
+++ TkUtils/main.c
@@ -0,0 +1,18 @@
+/*
+ * TkUtils.c
+ *
+ * Created: 03.05.2019 21:12:02
+ * Author : TK-A
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
